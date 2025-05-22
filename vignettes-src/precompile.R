d23bcceafa5456355f7a0aac97f6d7abbe32c4a2
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-05-22 09:08:11 +0100

    Update more vignettes

--- vignettes-src/precompile.R
+++ vignettes-src/precompile.R
@@ -1,6 +1,7 @@
 # Vignettes precompiled as require connecting to remote resources
 knitr::knit("vignettes-src/_mirai.Rmd", "vignettes/mirai.Rmd")
 knitr::knit("vignettes-src/_daemons.Rmd", "vignettes/daemons.Rmd")
+knitr::knit("vignettes-src/_map.Rmd", "vignettes/map.Rmd")
 knitr::knit("vignettes-src/_packages.Rmd", "vignettes/packages.Rmd")
 knitr::knit("vignettes-src/_parallel.Rmd", "vignettes/parallel.Rmd")
 knitr::knit("vignettes-src/_plumber.Rmd", "vignettes/plumber.Rmd")
