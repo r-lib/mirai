3322da7f79b5ad732205ea8cf3601c4bbb0a9227
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-06-24 16:41:13 +0100

    Fold dameons back into reference vignette, add concept overview

--- vignettes-src/precompile.R
+++ vignettes-src/precompile.R
@@ -1,6 +1,5 @@
-# Vignettes precompiled as require connecting to remote resources
+# Vignettes pre-compiled as require connecting to remote resources
 knitr::knit("vignettes-src/_mirai.Rmd", "vignettes/mirai.Rmd")
-knitr::knit("vignettes-src/_mirai-daemons.Rmd", "vignettes/mirai-daemons.Rmd")
 knitr::knit("vignettes-src/_mirai-map.Rmd", "vignettes/mirai-map.Rmd")
 knitr::knit("vignettes-src/_mirai-promises.Rmd", "vignettes/mirai-promises.Rmd")
 knitr::knit("vignettes-src/_mirai-serialization.Rmd", "vignettes/mirai-serialization.Rmd")
