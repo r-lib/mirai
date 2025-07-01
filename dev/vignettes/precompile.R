3e787ab0f7ad96c544f43f5d3cef7d0cee36fe6c
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-07-01 15:30:37 +0100

    Consolidate non-package folders under dev/

---
+++ dev/vignettes/precompile.R
@@ -0,0 +1,8 @@
+# Vignettes pre-compiled as require connecting to remote resources
+knitr::knit("dev/vignettes/_mirai.Rmd", "vignettes/mirai.Rmd")
+knitr::knit("dev/vignettes/_mirai-map.Rmd", "vignettes/mirai-map.Rmd")
+knitr::knit("dev/vignettes/_mirai-promises.Rmd", "vignettes/mirai-promises.Rmd")
+knitr::knit("dev/vignettes/_mirai-serialization.Rmd", "vignettes/mirai-serialization.Rmd")
+knitr::knit("dev/vignettes/_mirai-xparallel.Rmd", "vignettes/mirai-xparallel.Rmd")
+knitr::knit("dev/vignettes/_packages.Rmd", "vignettes/packages.Rmd")
+knitr::knit("dev/vignettes/_questions.Rmd", "vignettes/questions.Rmd")
--- vignettes-src/precompile.R
+++
@@ -1,8 +0,0 @@
-# Vignettes pre-compiled as require connecting to remote resources
-knitr::knit("vignettes-src/_mirai.Rmd", "vignettes/mirai.Rmd")
-knitr::knit("vignettes-src/_mirai-map.Rmd", "vignettes/mirai-map.Rmd")
-knitr::knit("vignettes-src/_mirai-promises.Rmd", "vignettes/mirai-promises.Rmd")
-knitr::knit("vignettes-src/_mirai-serialization.Rmd", "vignettes/mirai-serialization.Rmd")
-knitr::knit("vignettes-src/_mirai-xparallel.Rmd", "vignettes/mirai-xparallel.Rmd")
-knitr::knit("vignettes-src/_packages.Rmd", "vignettes/packages.Rmd")
-knitr::knit("vignettes-src/_questions.Rmd", "vignettes/questions.Rmd")
