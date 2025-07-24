d0f0d780b5faa3929d0db87afeda8d92e501ee8c
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-07-24 23:06:41 +0100

    Refactor a bit

--- R/launchers.R
+++ R/launchers.R
@@ -105,26 +105,10 @@ launch_remote <- function(
   if (length(remote) == 2L) {
     platform <- remote[["platform"]]
     args <- remote[["args"]]
-    platform == "posit" && {
-      tools <- posit_tools()
-      is.environment(tools) || stop(._[["posit_api"]])
-      submit_job <- .subset2(tools, ".rs.api.launcher.submitJob")
-      new_container <- .subset2(tools, ".rs.api.launcher.newContainer")
-      cluster <- args[["name"]]
-      container <- new_container(args[["image"]])
-      cmds <- launch_remote(n)
-      lapply(
-        cmds,
-        function(cmd) submit_job(
-          sprintf("mirai_daemon_%s", random(3L)),
-          cluster = cluster,
-          command = cmd,
-          container = container
-        )
-      )
-      return(cmds)
-    }
-    stop(._[["platform_unsupported"]])
+    platform != "posit" && stop(._[["platform_unsupported"]])
+    tools <- posit_tools()
+    is.environment(tools) || stop(._[["posit_api"]])
+    return(posit_workbench_launch(n, args, tools))
   }
 
   command <- remote[["command"]]
@@ -587,4 +571,21 @@ posit_tools <- function() {
   feature_available <- .subset2(tools, ".rs.api.launcher.jobsFeatureAvailable")
   is.function(feature_available) && feature_available() || return()
   tools
+}
+
+posit_workbench_launch <- function(n, args, tools) {
+  submit_job <- .subset2(tools, ".rs.api.launcher.submitJob")
+  new_container <- .subset2(tools, ".rs.api.launcher.newContainer")
+  cluster <- args[["name"]]
+  container <- new_container(args[["image"]])
+  cmds <- launch_remote(n)
+  lapply(cmds, function(cmd)
+    submit_job(
+      sprintf("mirai_daemon_%s", random(3L)),
+      cluster = cluster,
+      command = cmd,
+      container = container
+    )
+  )
+  cmds
 }
