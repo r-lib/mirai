77410ceba56a3e5cf0c77720fca55789f5503629
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-08-08 19:43:31 +0100

    Include daemon on which mirai is evaluated as an attribute

--- R/daemons.R
+++ R/daemons.R
@@ -259,7 +259,7 @@ daemons <- function(
 
       if (signal) send_signal(envir)
       reap(envir[["sock"]])
-      if (is_otel_tracing) {
+      if (otel_tracing) {
         envir[["otel_span"]][["end"]]()
       }
       ..[[.compute]] <- NULL -> envir
@@ -298,7 +298,7 @@ daemons <- function(
     )
   })
 
-  if (is_otel_tracing) {
+  if (otel_tracing) {
     envir[["otel_span"]] <- otel::start_span(
       "mirai::daemons",
       attributes = otel::as_attributes(list(compute_profile = .compute))
