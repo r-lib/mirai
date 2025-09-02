d98f254f4e9dd98e563f64fd1973f15240a88d8b
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-09-02 12:29:12 +0100

    Add opentelemetry vignette

--- dev/vignettes/precompile.R
+++ dev/vignettes/precompile.R
@@ -6,3 +6,4 @@ knitr::knit("dev/vignettes/_v03-serialization.Rmd", "vignettes/v03-serialization
 knitr::knit("dev/vignettes/_v04-parallel.Rmd", "vignettes/v04-parallel.Rmd")
 knitr::knit("dev/vignettes/_v05-packages.Rmd", "vignettes/v05-packages.Rmd")
 knitr::knit("dev/vignettes/_v06-questions.Rmd", "vignettes/v06-questions.Rmd")
+knitr::knit("dev/vignettes/_v07-opentelemetry.Rmd", "vignettes/v07-opentelemetry.Rmd")
