4c215f829ac68b406b6dd3c8212e76a3953cd5ca
Sam Albers <sam.albers@gmail.com>
2025-09-25 13:21:04 -0700

    add doc about cores

--- R/daemons.R
+++ R/daemons.R
@@ -6,7 +6,8 @@
 #' requests. Specify `n` to create daemons on the local machine. Specify `url`
 #' to receive connections from remote daemons (for distributed computing across
 #' the network). Specify `remote` to optionally launch remote daemons via a
-#' remote configuration. Dispatcher (enabled by default) ensures optimal
+#' remote configuration. For optimal performance, set `n` to one less than the
+#' number of available cores. Dispatcher (enabled by default) ensures optimal
 #' scheduling.
 #'
 #' Use `daemons(0)` to reset daemon connections:
