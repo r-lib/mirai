595f100968c2c202309e0af6e284aa72e14d769e
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-11-08 12:29:17 +0000

    Simplify `otel_cache_tracer()`

--- R/otel.R
+++ R/otel.R
@@ -1,46 +1,23 @@
 otel_tracer_name <- "org.r-lib.mirai"
 
-# generic otel helpers ---------------------------------------------------------
+# otel helpers -----------------------------------------------------------------
 
-otel_cache_tracer <- function() {
-  requireNamespace("otel", quietly = TRUE) || return()
-  tracer <- otel::get_tracer(otel_tracer_name)
-  list2env(
-    list(otel_tracer = tracer, otel_is_tracing = tracer_enabled(tracer)),
-    envir = environment(otel_active_span)
-  )
-}
-
-tracer_enabled <- function(tracer) {
-  .subset2(tracer, "is_enabled")()
-}
-
-with_otel_record <- function(expr) {
-  on.exit(otel_cache_tracer())
-  otelsdk::with_otel_record({
-    otel_cache_tracer()
-    expr
-  })
-}
-
-# mirai-specific helpers -------------------------------------------------------
-
+otel_cache_tracer <- NULL
+otel_active_span <- NULL
 otel_set_span_id <- NULL
 otel_set_span_error <- NULL
 
-otel_active_span <- local({
+local({
   otel_is_tracing <- FALSE
   otel_tracer <- NULL
 
-  otel_set_span_id <<- function(span, id) {
-    otel_is_tracing && return(span$set_attribute("mirai.id", id))
+  otel_cache_tracer <<- function() {
+    requireNamespace("otel", quietly = TRUE) || return()
+    otel_tracer <<- otel::get_tracer(otel_tracer_name)
+    otel_is_tracing <<- tracer_enabled(otel_tracer)
   }
 
-  otel_set_span_error <<- function(span, type) {
-    otel_is_tracing && inherits(span, "otel_span") && return(span$set_status("error", type))
-  }
-
-  function(
+  otel_active_span <<- function(
     name,
     cond = TRUE,
     attributes = list(),
@@ -61,7 +38,29 @@ otel_active_span <- local({
     return_ctx && return(list(otel::pack_http_context(), spn))
     spn
   }
+
+  otel_set_span_id <<- function(span, id) {
+    otel_is_tracing && return(span$set_attribute("mirai.id", id))
+  }
+
+  otel_set_span_error <<- function(span, type) {
+    otel_is_tracing && inherits(span, "otel_span") && return(span$set_status("error", type))
+  }
 })
+
+tracer_enabled <- function(tracer) {
+  .subset2(tracer, "is_enabled")()
+}
+
+with_otel_record <- function(expr) {
+  on.exit(otel_cache_tracer())
+  otelsdk::with_otel_record({
+    otel_cache_tracer()
+    expr
+  })
+}
+
+# mirai-specific helpers -------------------------------------------------------
 
 otel_daemon_attrs <- function(url) {
   purl <- parse_url(url)
