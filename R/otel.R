42181679e77557bc7163b8f52db34fe1e46c977c
Charlie Gao <53399081+shikokuchuo@users.noreply.github.com>
2025-11-10 20:52:53 +0000

    Update otel tracer caching strategy (#513)
    * Cache otel tracer in local function environment; add testing helper
    
    * Simplify `otel_cache_tracer()`
    
    * Can now skip `otel::as_attributes()`
    
    * Set error spans faster

--- R/otel.R
+++ R/otel.R
@@ -1,70 +1,66 @@
 otel_tracer_name <- "org.r-lib.mirai"
-otel_is_tracing <- FALSE
-otel_tracer <- NULL
 
-# generic otel helpers ---------------------------------------------------------
+# otel helpers -----------------------------------------------------------------
 
-# nocov start
-# tested implicitly on package load
+otel_cache_tracer <- NULL
+otel_active_span <- NULL
+otel_set_span_id <- NULL
+otel_set_span_error <- NULL
 
-otel_cache_tracer <- function() {
-  requireNamespace("otel", quietly = TRUE) || return()
-  otel_tracer <<- otel::get_tracer(otel_tracer_name)
-  otel_is_tracing <<- tracer_enabled(otel_tracer)
-}
+local({
+  otel_is_tracing <- FALSE
+  otel_tracer <- NULL
 
-# nocov end
+  otel_cache_tracer <<- function() {
+    requireNamespace("otel", quietly = TRUE) || return()
+    otel_tracer <<- otel::get_tracer(otel_tracer_name)
+    otel_is_tracing <<- tracer_enabled(otel_tracer)
+  }
+
+  otel_active_span <<- function(
+    name,
+    cond = TRUE,
+    attributes = NULL,
+    links = NULL,
+    options = NULL,
+    return_ctx = FALSE,
+    scope = environment()
+  ) {
+    otel_is_tracing && cond || return()
+    spn <- otel::start_local_active_span(
+      name,
+      attributes = attributes,
+      links = links,
+      options = options,
+      tracer = otel_tracer,
+      activation_scope = scope
+    )
+    return_ctx && return(list(otel::pack_http_context(), spn))
+    spn
+  }
+
+  otel_set_span_id <<- function(span, id) {
+    otel_is_tracing && return(span$set_attribute("mirai.id", id))
+  }
+
+  otel_set_span_error <<- function(span, type) {
+    otel_is_tracing && is.environment(span) && return(span$set_status("error", type))
+  }
+})
 
 tracer_enabled <- function(tracer) {
   .subset2(tracer, "is_enabled")()
 }
 
-otel_refresh_tracer <- function() {
-  requireNamespace("otel", quietly = TRUE) || return()
-  tracer <- otel::get_tracer()
-  modify_binding(
-    topenv(),
-    list(otel_tracer = tracer, otel_is_tracing = tracer_enabled(tracer))
-  )
-}
-
-modify_binding <- function(env, lst) {
-  lapply(names(lst), unlockBinding, env)
-  list2env(lst, envir = env)
-  lapply(names(lst), lockBinding, env)
+with_otel_record <- function(expr) {
+  on.exit(otel_cache_tracer())
+  otelsdk::with_otel_record({
+    otel_cache_tracer()
+    expr
+  })
 }
 
 # mirai-specific helpers -------------------------------------------------------
-
-otel_active_span <- function(
-  name,
-  cond = TRUE,
-  attributes = list(),
-  links = NULL,
-  options = NULL,
-  return_ctx = FALSE,
-  scope = environment()
-) {
-  otel_is_tracing && cond || return()
-  spn <- otel::start_local_active_span(
-    name,
-    attributes = otel::as_attributes(attributes),
-    links = links,
-    options = options,
-    tracer = otel_tracer,
-    activation_scope = scope
-  )
-  return_ctx && return(list(otel::pack_http_context(), spn))
-  spn
-}
-
-otel_set_span_id <- function(span, id) {
-  otel_is_tracing && return(span$set_attribute("mirai.id", id))
-}
-
-otel_set_span_error <- function(span, type) {
-  otel_is_tracing && inherits(span, "otel_span") && return(span$set_status("error", type))
-}
 
 otel_daemon_attrs <- function(url) {
   purl <- parse_url(url)
