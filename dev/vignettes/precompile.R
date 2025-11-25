1ed3025a2024e6dee277c0983511e986a277a81f
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-11-25 23:03:26 +0000

    Reorganize vignettes

--- dev/vignettes/precompile.R
+++ dev/vignettes/precompile.R
@@ -1,6 +1,5 @@
 # Vignettes pre-compiled as require connecting to remote resources
-knitr::knit("dev/vignettes/_v00-mirai.Rmd", "vignettes/v00-mirai.Rmd")
-knitr::knit("dev/vignettes/_v01-map.Rmd", "vignettes/v01-map.Rmd")
+knitr::knit("dev/vignettes/_v01-reference.Rmd", "vignettes/v01-reference.Rmd")
 knitr::knit("dev/vignettes/_v02-promises.Rmd", "vignettes/v02-promises.Rmd")
 knitr::knit("dev/vignettes/_v03-serialization.Rmd", "vignettes/v03-serialization.Rmd")
 knitr::knit("dev/vignettes/_v04-parallel.Rmd", "vignettes/v04-parallel.Rmd")
