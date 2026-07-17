788d936ac15abfa8da38a7f4e44346906b38c919
Charlie Gao <53399081+shikokuchuo@users.noreply.github.com>
2026-07-17 11:51:28 +0100

    Fix .flat ignoring an errorValue in the first map element (#640)
    * Fix .flat ignoring an errorValue in the first map element
    
    * Relax .flat error tests to not match cli messages
    
    * Retain only the discriminating .flat regression test

--- tests/tests.R
+++ tests/tests.R
@@ -214,6 +214,7 @@ connection && {
   test_true(all(mres == 7L))
   test_null(names(mres))
   test_true(all(mirai_map(list(c(a = 1, b = 1, c = 1), 3), sum)[.flat] == 3))
+  test_error(mirai_map(list("a"), function(x) x + 1L)[.flat])
   test_type("language", mirai_map(list(quote(1+2)), identity)[][[1]])
   test_class("Date", mirai_map(data.frame(x = as.Date("2020-01-01")), identity)[][[1]])
   test_true(is_mirai_error(mirai_map(1:2, function(x) daemons(1))[][[1]]))
